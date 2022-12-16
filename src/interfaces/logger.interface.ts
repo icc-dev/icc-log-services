0ea04add30ff215f3e11e21adab14425d83d63e0
IanCardenasCastillo <iancardenas96@gmail.com>
2022-12-16 18:24:37 -0300

    modify(logger.interface): remove function

--- src/interfaces/logger.interface.ts
+++ src/interfaces/logger.interface.ts
@@ -1,9 +1,6 @@
-import { IDefaultMetaService } from "./metadata.interface";
-
 export interface ILogger {
     log(message: string, aditionalData?: any): void;
     warn(message: string, aditionalData?: any): void;
     error(message: string, aditionalData?: any): void;
     debug(message: string, aditionalData?: any): void;
-    addMetadata(metadata: IDefaultMetaService): void;
 }
